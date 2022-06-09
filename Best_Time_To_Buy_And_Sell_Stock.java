de3dea17c29d330f4b0b4927574b312f28c22d6f
Singh0701 <58650049+Singh0701@users.noreply.github.com>
2022-06-09 14:47:29 +0530

    Update Best_Time_To_Buy_And_Sell_Stock.java

--- Best_Time_To_Buy_And_Sell_Stock.java
+++ Best_Time_To_Buy_And_Sell_Stock.java
@@ -1,5 +1,7 @@
 // Problem Statement:
-// You are given an array/list 'prices' where the elements of the array represent the prices of the stock as they were yesterday and indices of the array represent minutes. Your task is to find and return the maximum profit you can make by buying and selling the stock. You can buy and sell the stock only once.
+// You are given an array/list 'prices' where the elements of the array represent the prices of the stock as they were yesterday and indices of the 
+//array represent minutes. Your task is to find and return the maximum profit you can make by buying and selling the stock. You can buy and sell 
+//the stock only once.
 // Note: You can’t sell without buying first.
 // For Example:
 // For the given array [ 2, 100, 150, 120],
