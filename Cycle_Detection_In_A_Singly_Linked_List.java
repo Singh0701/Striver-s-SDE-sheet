eb9d88db1d5a7b77c9f0cf900bd1a982398e4c19
Singh0701 <58650049+Singh0701@users.noreply.github.com>
2022-06-13 17:32:35 +0530

    Update Cycle_Detection_In_A_Singly_Linked_List.java

--- Cycle_Detection_In_A_Singly_Linked_List.java
+++ Cycle_Detection_In_A_Singly_Linked_List.java
@@ -1,13 +1,12 @@
 // Problem Statement
 // You have given a Singly Linked List of integers, determine if it forms a cycle or not.
 // A cycle occurs when a node's next points back to a previous node in the list. The linked list is no longer linear with a beginning and end—instead, it cycles through a loop of nodes.
-// Note: Since, it is binary problem, there is no partial marking. Marks will only be awarded if you get all the test cases correct.
 // Input Format :
 // The first line of each test case contains the elements of the singly linked list separated by a single space and terminated by -1 and hence -1 would never be a list element.
 
 // The second line contains the integer position "pos" which represents the position (0-indexed) in the linked list where tail connects to. If "pos" is -1, then there is no cycle in the linked list.
 // Output Format :
-// The only line of output contains 'true' if linked list has a cycle or 'false' otherwise.
+// The only line of output contains 'true' if the linked list has a cycle or 'false' otherwise.
 
 // You don't have to explicitly print by yourself. It has been taken care of.
 // Constraints :
@@ -24,10 +23,10 @@
 
 //Solution:
 
-//Approach 1: Using Set data structure, we'll keep adding nodes into set until head becomes null or if we found an node which is already in the set then return true. (As set doesn't allows duplicates).
+//Approach 1: Using Set data structure, we'll keep adding nodes into the set until head becomes null or if we found a node which is already in the set then return true. (As set doesn't allow duplicates).
 
 import java.util.* ;
-import java.io.*; 
+import java.io.*;
 /*  
 
     Following is the representation of the Singly Linked List node
@@ -45,7 +44,7 @@ import java.io.*;
 */
 
 public class Solution {
-    
+   
     public static boolean detectCycle(Node<Integer> head) {
         //Create a Set to store nodes.
         Set<Node> set = new HashSet<>();
@@ -59,10 +58,44 @@ public class Solution {
             //Move head to next node.
             head = head.next;
         }
-        //Return false if we didn't found any cycle while running the above snippet of code.
+        //Return false if we didn't find any cycle while running the above snippet of code.
         return false;
     }
 }
 
 //Time complexity = O(N)
 //Space complexity = O(N)
+
+
+//Approach 2: (Optimal approach) Using slow and a fast pointer. That is, we'll take slow and fast pointers and set initially to head. Now will run a while loop while the fast is not equal to null (Because if the cycle is not present then fast pointer will reach the last node first and get the null value). Inside loop will first move the slow pointer to next node and fast pointer to the next of the next node and then check if the slow is pointing to the same node as fast pointer (that is slow == fast) Which means cycle is present in the linked list, So return true. Else false outside the loop.
+
+import java.util.* ;
+import java.io.*;
+
+public class Solution {
+   
+    public static boolean detectCycle(Node<Integer> head) {
+        //Check if the list is empty or just one node is present, then no cycle can be found! Return false.
+        if(head == null || head.next == null) return false;
+        //Take slow and fast pointers, initialize to head.
+        Node slow = head, fast = head;
+        //Iterate over the list while fast pointer is not equal to null.
+        while(fast != null) {
+            //Move slow pointer by one.
+            slow = slow.next;
+            //Check if the fast is at the last node and return false, otherwise it'll show an null pointer exception error for accessing null.next.
+            if(fast.next == null) return false;
+            //Move fast.
+            fast = fast.next.next;
+            //If the slow and fast are pointing to the same node, which implies that there is a cycle present in the linked list, so return true.
+            if(slow == fast)
+                return true;
+        }
+        //Return false as no cycle found.
+        return false;
+    }
+}
+
+
+//Time complexity = O(N)
+//Space complexity = O(1)
