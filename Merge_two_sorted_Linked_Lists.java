79e6c80532b8f024ed0ada0bd941452c2263258b
Singh0701 <58650049+Singh0701@users.noreply.github.com>
2022-07-10 17:28:03 +0530

    Update Merge_two_sorted_Linked_Lists.java

--- Merge_two_sorted_Linked_Lists.java
+++ Merge_two_sorted_Linked_Lists.java
@@ -55,3 +55,35 @@
 // Where N and M are size of list1 and list2 respectively.  
 
 
+// Approach 1: Another approach (similar to above in terms of time and space complexities).
+
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        if(list1.val > list2.val) {
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+        ListNode result = list1;
+        while(list1 != null && list2 != null) {
+            ListNode prev = list1;
+            while(list1 != null && list1.val <= list2.val) {
+                prev = list1;
+                list1 = list1.next;
+            }
+            prev.next = list2;
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+        return result;
+    }
+}
+
+
+
+// Time complexity = O(N+M)
+// Space complexity = O(1)
