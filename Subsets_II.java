0bcfff8f0aea2978575b5eafec18663720375146
Singh0701 <58650049+Singh0701@users.noreply.github.com>
2022-07-21 14:32:42 +0530

    Update Subsets_II.java

--- Subsets_II.java
+++ Subsets_II.java
@@ -46,7 +46,7 @@ class Solution {
 }
 
 
-//Time complexity = O(2^n * k)
-//assuming k as the avg length of each subset we generate and it'll take O(k) time complexity to make a copy of the subset.
+//Time complexity = O(2^n * k) + O(n*log(n))
+//assuming k as the avg length of each subset we generate and it'll take O(k) time complexity to make a copy of the subset, and O(n*log(n)) for sorting array initially.
 //Space complexity = O(2^n * k) + O(n)
 //K is the Avg length of each subset, O(n) will be the stack space for recursion calls.
