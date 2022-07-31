5eb833150fecc55fb68e22f05eda4cba9ed2ba94
Singh0701 <58650049+Singh0701@users.noreply.github.com>
2022-07-31 14:10:22 +0530

    Update Ninja’s Training.java

--- Ninja’s Training.java
+++ Ninja’s Training.java
@@ -76,3 +76,51 @@ public class Solution {
         return maxPoints;
     }
 }
+
+//Approach 2: Using Memoization.
+
+
+import java.util.*;
+public class Solution {
+    public static int ninjaTraining(int n, int points[][]) {
+        //Make a 2D array to store the overlapping ssub-problems.
+        int[][] dp = new int[n][4];
+        return getMax(n - 1, points, 3, dp);
+    }
+    public static int getMax(int day, int points[][], int lastTask, int[][] dp) {
+        //Base Case
+        if(day == 0) {
+            //Try to select task except the last one, which gives maximum points.
+            int maxi = 0;
+            //Try for all 3 tasks, and check if it is same as task performed on the previous day or not.
+            for (int task = 0; task < 3; task++) {
+                if (task != lastTask)
+                    maxi = Math.max(maxi, points[0][task]);
+            }
+            //Store the result in dp array.
+            return dp[day][lastTask] = maxi;
+        }
+        //Check if this computation is already done. If yes then return it, else compute it and then store it into the array.
+        if(dp[day][lastTask] != 0) return dp[day][lastTask];
+        //For day 1 to n - 1.
+        int maxPoints = 0;
+        //Try out all 3 tasks and check if it is same as task performed on the previous day or not.
+        for(int task = 0; task < 3; task++) {
+            if(task != lastTask) {
+                //Take the points of the selected task and go to previous day and make sure to pass on the task selected as last parameter in function call.
+                int point = points[day][task] + getMax(day - 1, points, task, dp);
+                maxPoints = Math.max(maxPoints, point);
+            }
+            //Take the maximum of all.
+        }
+        //Return the Maximum Points obtained.
+        return dp[day][lastTask] = maxPoints;
+    }
+}
+
+
+
+//Approach 3: Using Tabulation.
+
+
+
