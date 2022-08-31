4bfa6d7e27ccedb848221978e7c3bc9a5efac00a
Devender Singh <58650049+Singh0701@users.noreply.github.com>
2022-09-01 00:13:30 +0530

    Update Partition a set into two subsets such that the difference of subset sums is minimum.java

--- Partition a set into two subsets such that the difference of subset sums is minimum.java
+++ Partition a set into two subsets such that the difference of subset sums is minimum.java
@@ -19,3 +19,39 @@ public class Solution {
         return Math.min(take, notTake);
     }
 }
+
+
+
+//Approach 2: Using Tabulation and intuition from the problem subset sum equals.
+public class Solution {
+	public static int minSubsetSumDifference(int[] arr, int n) {
+        int sum = 0;
+        for(int i: arr) sum += i;
+		boolean[] prev = new boolean[sum + 1];
+        if(arr[0] <= sum) prev[arr[0]] = true;
+        for(int i = 1; i < n; i++) {
+            boolean[] curr = new boolean[sum + 1];
+            curr[0] = true;
+            for(int j = 1; j <= sum; j++) {
+                boolean take = (j >= arr[i]) ? prev[j - arr[i]] : false;
+                boolean notTake = prev[j];
+                curr[j] = take || notTake;
+            }
+            prev = curr;
+        }
+        int min = (int) 1e18;
+        for(int i = 0; i <= sum; i++) {
+            if(prev[i]) {
+                min = Math.min(min, Math.abs(i - (sum - i)));
+            }
+        }     
+        return min;
+	}
+    public static int f(int index, int[] arr, int sum1, int sum2) {
+        if(index < 0) return Math.abs(sum1 - sum2);
+        int take = f(index - 1, arr, sum1 - arr[index], sum2 + arr[index]);
+        int notTake = f(index - 1, arr, sum1, sum2);
+        return Math.min(take, notTake);
+    }
+}
+
