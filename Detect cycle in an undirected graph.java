a98a6302eb18629c3d4dd8310aa7936c7ce2d1a0
Devender Singh <58650049+Singh0701@users.noreply.github.com>
2022-09-05 01:31:13 +0530

    Update Detect cycle in an undirected graph.java

--- Detect cycle in an undirected graph.java
+++ Detect cycle in an undirected graph.java
@@ -1,36 +1,4 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            boolean ans = obj.isCycle(V, adj);
-            if (ans)
-                System.out.println("1");
-            else
-                System.out.println("0");
-        }
-    }
-}
-// } Driver Code Ends
-
+//BFS
 
 class Solution {
     
@@ -67,6 +35,34 @@ class Solution {
                     visited[n] = true;
                 } else if(n != parent) return true;
             }
+        }
+        return false;
+    }
+}
+
+
+//DFS  
+
+
+class Solution {
+    
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+        for(int i = 0; i < V; i++)
+            if(!visited[i]) {
+                if(dfs(i, -1, adj, visited)) return true;
+            }
+        return false;
+    }
+    
+    public boolean dfs(int sr, int parent, ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
+        visited[sr] = true;
+        for(int n: adj.get(sr)) {
+            if(!visited[n]) {
+                if(dfs(n, sr, adj, visited))
+                    return true;
+            } else if(n != parent) return true;
         }
         return false;
     }
