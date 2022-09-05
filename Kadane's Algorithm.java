84508530c08d27d819abf754aa680569c36f9d56
Devender Singh <58650049+Singh0701@users.noreply.github.com>
2022-09-05 20:49:42 +0530

    Update Kadane's Algorithm.java

--- Kadane's Algorithm.java
+++ Kadane's Algorithm.java
@@ -1,3 +1,46 @@
+//Brute Force.
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                int curr = 0;
+                for(int k = i; k <= j; k++) {
+                    curr += nums[k];
+                }
+                maxSum = Math.max(curr, maxSum);
+            }
+        }
+        return maxSum;
+    }
+}
+
+//Time complexity = O(N^3)
+//Space complexity = O(1)
+
+
+//Better Approach:
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            int curr = 0;
+            for(int j = i; j < n; j++) {
+                curr += nums[j];
+                maxSum = Math.max(curr, maxSum);
+            }
+        }
+        return maxSum;
+    }
+}
+
+//Time complexity = O(N^2)
+//Space complexity = O(1)
+
 import java.util.*;
 import java.io.*; 
 
@@ -22,4 +65,4 @@ public class Solution {
 
 
 //Time complexity = O(N)
-//Space complexity = O(N)
+//Space complexity = O(1)
